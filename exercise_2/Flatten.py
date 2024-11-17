9e46f063bf04f8599ba65198bced073272f319f7
uq88acaw <norul.gofran@fau.de>
2024-11-17 15:11:11 +0100

    Add Flatten layer implementation with forward and backward methods

---
+++ exercise_2/Flatten.py
@@ -0,0 +1,15 @@
+import numpy as np
+from Layers.Base import BaseLayer
+
+
+class Flatten(BaseLayer):
+    def __init__(self):
+        super().__init__()
+        self.input_tensor = None
+
+    def forward(self, input_tensor):
+        self.input_tensor = input_tensor
+        return input_tensor.reshape(len(input_tensor), input_tensor[0].flatten().size)
+
+    def backward(self, error_tensor):
+        return error_tensor.reshape(self.input_tensor.shape)
